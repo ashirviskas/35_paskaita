eb4413825d09b1aeac70701aaf4f4811f66a3ca5
Matas Minelga <minematas@gmail.com>
2021-08-03 18:48:18 +0300

    Paruostas projektas

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
+import secrets
+from PIL import Image
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, current_user, logout_user, login_user, UserMixin, login_required
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 import forms
+from datetime import datetime
+from sqlalchemy import DateTime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -17,12 +23,42 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'prisijungti'
 login_manager.login_message_category = 'info'
 
+class ManoModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.el_pastas == "el@pastas.lt"
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profilio_nuotraukos', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
 class Vartotojas(db.Model, UserMixin):
     __tablename__ = "vartotojas"
     id = db.Column(db.Integer, primary_key=True)
     vardas = db.Column("Vardas", db.String(20), unique=True, nullable=False)
     el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
+    nuotrauka = db.Column(db.String(20), nullable=False, default='default.jpg')
     slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
+
+class Irasas(db.Model):
+    __tablename__ = "irasas"
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column("Data", DateTime, default=datetime.now())
+    pajamos = db.Column("Pajamos", db.Boolean)
+    suma = db.Column("Vardas", db.Integer)
+    vartotojas_id = db.Column(db.Integer, db.ForeignKey("vartotojas.id"))
+    vartotojas = db.relationship("Vartotojas", lazy=True)
+
+admin = Admin(app)
+# admin.add_view(ModelView(Irasas, db.session))
+admin.add_view(ManoModelView(Vartotojas, db.session))
 
 @login_manager.user_loader
 def load_user(vartotojo_id):
@@ -65,15 +101,86 @@ def atsijungti():
     return redirect(url_for('index'))
 
 
-@app.route("/paskyra")
+
+@app.route("/paskyra", methods=['GET', 'POST'])
 @login_required
-def account():
-    return render_template('paskyra.html', title='Paskyra')
+def paskyra():
+    form = forms.PaskyrosAtnaujinimoForma()
+    if form.validate_on_submit():
+        if form.nuotrauka.data:
+            nuotrauka = save_picture(form.nuotrauka.data)
+            current_user.nuotrauka = nuotrauka
+        current_user.vardas = form.vardas.data
+        current_user.el_pastas = form.el_pastas.data
+        db.session.commit()
+        flash('Tavo paskyra atnaujinta!', 'success')
+        return redirect(url_for('paskyra'))
+    elif request.method == 'GET':
+        form.vardas.data = current_user.vardas
+        form.el_pastas.data = current_user.el_pastas
+    nuotrauka = url_for('static', filename='profilio_nuotraukos/' + current_user.nuotrauka)
+    return render_template('paskyra.html', title='Account', form=form, nuotrauka=nuotrauka)
 
 @app.route("/irasai")
 @login_required
-def irasai():
-    return render_template('irasai.html', title='Įrašai')
+def records():
+    db.create_all()
+    page = request.args.get('page', 1, type=int)
+    puslapio_irasai = Irasas.query.filter_by(vartotojas_id=current_user.id).order_by(Irasas.data.desc()).paginate(page=page, per_page=3)
+    return render_template("irasai.html", visi_irasai=puslapio_irasai, datetime=datetime)
+
+
+
+@app.route("/naujas_irasas", methods=["GET", "POST"])
+@login_required
+def new_record():
+    db.create_all()
+    forma = forms.IrasasForm()
+    if forma.validate_on_submit():
+        naujas_irasas = Irasas(pajamos=forma.pajamos.data, suma=forma.suma.data, vartotojas_id=current_user.id)
+        db.session.add(naujas_irasas)
+        db.session.commit()
+        flash(f"Įrašas sukurtas", 'success')
+        return redirect(url_for('records'))
+    return render_template("prideti_irasa.html", form=forma)
+
+
+@app.route("/delete/<int:id>")
+@login_required
+def delete(id):
+    irasas = Irasas.query.get(id)
+    db.session.delete(irasas)
+    db.session.commit()
+    return redirect(url_for('records'))
+
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+@login_required
+def update(id):
+    forma = forms.IrasasForm()
+    irasas = Irasas.query.get(id)
+    if forma.validate_on_submit():
+        irasas.pajamos = forma.pajamos.data
+        irasas.suma = forma.suma.data
+        db.session.commit()
+        return redirect(url_for('records'))
+    return render_template("update.html", form=forma, irasas=irasas)
+
+
+@app.route("/balansas")
+@login_required
+def balance():
+    try:
+        visi_irasai = Irasas.query.filter_by(vartotojas_id=current_user.id)
+    except:
+        visi_irasai = []
+    balansas = 0
+    for irasas in visi_irasai:
+        if irasas.pajamos:
+            balansas += irasas.suma
+        else:
+            balansas -= irasas.suma
+    return render_template("balansas.html", balansas=balansas)
 
 
 @app.route("/")
--- forms.py
+++ forms.py
@@ -1,7 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, BooleanField, StringField, PasswordField
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, FloatField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, ValidationError, EqualTo
 import app
+
+
+class PaskyrosAtnaujinimoForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    nuotrauka = FileField('Atnaujinti profilio nuotrauką', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Atnaujinti')
+
+    def tikrinti_varda(self, vardas):
+        if vardas.data != app.current_user.vardas:
+            vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+
+    def tikrinti_pasta(self, el_pastas):
+        if el_pastas.data != app.current_user.el_pastas:
+            vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
 
 
 class RegistracijosForma(FlaskForm):
@@ -27,3 +47,9 @@ class PrisijungimoForma(FlaskForm):
     slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
     prisiminti = BooleanField("Prisiminti mane")
     submit = SubmitField('Prisijungti')
+
+
+class IrasasForm(FlaskForm):
+    pajamos = BooleanField('Pajamos')
+    suma = FloatField('Suma', [DataRequired()])
+    submit = SubmitField('Įvesti')
